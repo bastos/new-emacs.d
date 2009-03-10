127fbbd6c2f409d39f14112b9ae362e5c2f9592c
Tiago Bastos <comechao@gmail.com>
2009-03-10 17:37:11 +0100

    Added support to textmate and gist

--- main.el
+++ main.el
@@ -1,6 +1,11 @@
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
- 
-(add-to-list 'load-path dotfiles-dir)
 
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path "gist.el")
+(add-to-list 'load-path "textmate.el")
 (require 'ruby-init)
+(require 'find-recursive)
+(require 'gist)
+(require 'textmate)
+(textmate-mode)
